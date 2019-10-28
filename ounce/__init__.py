ef36aff3d9206e1af0c211082d6fb01183c809e6
Paul Melnikow <email@paulmelnikow.com>
2019-10-28 11:16:23 -0400

    Add lintignores

--- ounce/__init__.py
+++ ounce/__init__.py
@@ -1,5 +1,5 @@
-from .package_version import __version__
-from .core import *
+from .package_version import __version__  # noqa: F401
+from .core import *  # noqa: F401, F403
 from . import core as _core
 
 __all__ = _core.__all__
