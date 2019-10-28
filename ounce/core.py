936f948aab79a5bd836b0cab58fa88a8288e2316
Paul Melnikow <email@paulmelnikow.com>
2019-10-28 11:13:54 -0400

    Import round_to from blmath

--- ounce/core.py
+++ ounce/core.py
@@ -236,9 +236,34 @@ def prettify(value, units, precision=None):
 
     """
 
-    def round_value(value, units):
-        from blmath.numerics import round_to
+    def round_to(value, nearest):
+        """
+        Round a number to the nearest specified increment.
 
+        e.g.:
+
+            >>> round_to(3.8721, 0.05)
+            3.85
+            >>> round_to(3.8721, 0.1)
+            3.9
+            >>> round_to(3.8725, 0.25)
+            3.75
+            >>> round_to(3.8725, 2.0)
+            4
+
+        Use reciprocal due to floating point weirdness:
+
+            >>> value, nearest = 3.9, 0.1
+            >>> round(value / nearest) * nearest
+            3.9000000000000004
+            >>> round(value / nearest) / (1.0 / nearest)
+            3.9
+
+        """
+        reciprocal = 1.0 / nearest
+        return round(value / nearest) / reciprocal
+
+    def round_value(value, units):
         if precision is None:
             nearest = {"cm": 0.5, "in": 0.25, "kg": 0.5, "lb": 1.0, "yr": 1.0}.get(
                 units, 0.1
